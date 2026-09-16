b4a8cf18f02dae3692ce79e996c448f549b3c8cd
Kamal Kumar <141873182+kamalkumarx@users.noreply.github.com>
2026-09-17 02:26:04 +0530

    Update ad-config.js

--- ad-config.js
+++ ad-config.js
@@ -1,8 +1,8 @@
 // Change only these values when you want to replace the advertisement.
 // The external website must allow iframe embedding and use HTTPS.
 window.GOLDEN_AD_CONFIG = {
-  adPageUrl: "https://celebrated-pegasus-e71092.netlify.app/",
-  sponsorWebsiteUrl: "https:/celebrated-pegasus-e71092.netlify.app/",
+  adPageUrl: "https://sample/",
+  sponsorWebsiteUrl: "https://sample/",
   durationSeconds: 30,
   ctaAppearsAfterSeconds: 10
 };
